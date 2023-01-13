bb504faa84f8eac6fd54c185a70426931a131db7
ThilakReddy00 <121423207+ThilakReddy00@users.noreply.github.com>
2023-01-13 11:20:27 +0300

    My Website

---
+++ script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing", {
+    strings:["","Senior Test Engineer", "Web Developer", "Web Designer", "Senior Test Engineer & Web Developer"],
+    typeSpeed:200,
+    BackSpeed:100,
+    loop:true
+})
